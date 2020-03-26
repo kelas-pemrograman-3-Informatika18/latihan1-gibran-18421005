76d52ece13da1de99353679d43058991899b2ee5
unknown <gallant.18421005@student.ubl.ac.id>
2020-03-26 15:29:25 +0700

    first commit

---
+++ src/pages/Register.vue
@@ -0,0 +1,72 @@
+<template>
+    <q-page>
+        Halaman Register
+    <q-form
+      @submit="onSubmit"
+      @reset="onReset"
+      class="q-gutter-md"
+    >
+      <q-input
+        filled
+        v-model="name"
+        label="Nama Anda"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type your name']"
+      />
+
+      <q-input
+        filled
+        v-model="username"
+        label="username Anda"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type username']"
+      />
+
+      <q-input
+        filled
+        type="password"
+        v-model="password"
+        label="password anda"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'Please type your password',
+        ]"
+      />
+
+      <q-input
+        filled
+        type="password"
+        v-model="rpassword"
+        label="ulangi password anda"
+        lazy-rules
+        :rules="[
+          val => val !== null && val !== '' || 'Please type your password',
+        ]"
+      />
+      <div>
+        <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+    </q-page>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      name: null,
+      username: null,
+      password: null,
+      rpassword: null
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('ini fungsi submit')
+    },
+    onReset () {
+      console.log('in fungsi reset')
+    }
+  }
+}
+</script>
---
+++ src/router/routes.js
@@ -0,0 +1,28 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/LoginLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Login.vue') },
+      { path: 'Register', component: () => import('pages/Register.vue') }
+    ]
+  },
+  {
+    path: '/home',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/index.vue') }
+    ]
+  }
+]
+
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+  routes.push({
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  })
+}
+
+export default routes
